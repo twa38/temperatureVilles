af0c4a115c58c6d8bf8a99eade522ecbb7588575
Warnier <thomas.warnier38@gmail.com>
2021-01-26 21:22:02 +0100

    ajout pression

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <body>
 
 <?php
+//test git
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=bdd_temperaturevilles;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
